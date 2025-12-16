d44094f9e9b7484276f25fd54c72326f6a5fc523
Roman-Maliarenko <romamalarenko1@gmail.com>
2025-12-16 16:04:22 +0100

    completed lesson10

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -1,5 +1,6 @@
 /*
-  Цель задания: Разработать функционал для удаления фильма из списка с использованием паттерна MVC. После удаления фильма, необходимо отобразить сообщение "Фильм успешно удалён!" в message-box
+  Цель задания: Разработать функционал для удаления фильма из списка с использованием паттерна MVC. После удаления фильма,
+  необходимо отобразить сообщение "Фильм успешно удалён!" в message-box
 
   При возникновении сложностей можете ознакомиться с пошаговым планом реализации ниже, но лучше попробовать сначала самостоятельно 🧙‍♂️
 
@@ -22,79 +23,101 @@
 */
 
 const model = {
-  movies: [],
-  addMovie(title, description) {
-    const id = Math.random()
-    const newMovie = { id, title, description }
-    this.movies.push(newMovie)
-    view.renderMovies(this.movies)
-  },
-  // your code
+    movies: [],
+    addMovie(title, description) {
+        const id = Math.random()
+        const newMovie = {id, title, description}
+        this.movies.push(newMovie)
+        view.renderMovies(this.movies)
+    },
+    // your code
+    deleteMovie(MovieId) {
+        this.movies = this.movies.filter((movie) => movie.id !== MovieId)
+        view.renderMovies(this.movies)
+    },
+
 }
 
 const view = {
-  init() {
-    this.renderMovies(model.movies)
+    init() {
+        this.renderMovies(model.movies)
 
-    const form = document.querySelector('.form')
-    const inputTitle = document.querySelector('.input-title')
-    const inputDescription = document.querySelector('.input-description')
+        const form = document.querySelector('.form')
+        const inputTitle = document.querySelector('.input-title')
+        const inputDescription = document.querySelector('.input-description')
 
-    form.addEventListener('submit', function (event) {
-      event.preventDefault()
-      const title = inputTitle.value
-      const description = inputDescription.value
-      controller.addMovie(title, description)
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            const title = inputTitle.value
+            const description = inputDescription.value
+            controller.addMovie(title, description)
 
-      inputTitle.value = ''
-      inputDescription.value = ''
-    })
+            inputTitle.value = ''
+            inputDescription.value = ''
+        })
 
-    // your code
-  },
-  renderMovies(movies) {
-    const list = document.querySelector('.list')
-    let moviesHTML = ''
+        // your code
+        const list = document.querySelector('.list')
 
-    for (const movie of movies) {
-      moviesHTML += `
+        list.addEventListener('click', function (event) {
+            if (event.target.classList.contains('delete-button')) {
+                const MovieId = +event.target.parentElement.id
+                controller.deleteMovie(MovieId)
+            }
+        })
+
+    },
+    renderMovies(movies) {
+        const list = document.querySelector('.list')
+        let moviesHTML = ''
+
+        for (const movie of movies) {
+            moviesHTML += `
         <li id="${movie.id}" class="movie">
           <b class="movie-title">${movie.title}</b>
           <p class="movie-description">${movie.description}</p>
           <button class="delete-button" type="button">Удалить 🗑</button>
         </li>
       `
-    }
+        }
 
-    list.innerHTML = moviesHTML
-  },
-  displayMessage(message, isError = false) {
-    const messageBox = document.querySelector('.message-box')
-    messageBox.textContent = message
-    if (isError) {
-      messageBox.classList.remove('success')
-      messageBox.classList.add('error')
-    } else {
-      messageBox.classList.remove('error')
-      messageBox.classList.add('success')
-    }
-  },
+        list.innerHTML = moviesHTML
+    },
+
+    displayMessage(message, isError = false) {
+        const messageBox = document.querySelector('.message-box')
+        messageBox.textContent = message
+        if (isError) {
+            messageBox.classList.remove('success')
+            messageBox.classList.add('error')
+        } else {
+            messageBox.classList.remove('error')
+            messageBox.classList.add('success')
+        }
+    },
 }
 
 const controller = {
-  addMovie(title, description) {
-    if (title.trim() !== '' && description.trim() !== '') {
-      model.addMovie(title, description)
-      view.displayMessage('Фильм добавлен успешно!')
-    } else {
-      view.displayMessage('Заполните все поля!', true)
-    }
-  },
-  // your code
+    addMovie(title, description) {
+        if (title.trim() !== '' && description.trim() !== '') {
+            model.addMovie(title, description)
+            view.displayMessage('Фильм добавлен успешно!')
+        } else {
+            view.displayMessage('Заполните все поля!', true)
+        }
+    },
+
+// your code
+
+    deleteMovie(MovieId) {
+        model.deleteMovie(MovieId)
+        view.displayMessage('Фильм успешно удален!')
+    },
+
 }
 
 function init() {
-  view.init()
+    view.init()
 }
 
 document.addEventListener('DOMContentLoaded', init)
